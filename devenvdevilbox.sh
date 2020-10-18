8c4f6b5c89c18cf6516e960b502580a6cce2ec7e
Jonatan <web-jonatan@live.com>
2020-10-17 23:29:21 -0300

    Update devenvdevilbox.sh

--- devenvdevilbox.sh
+++ devenvdevilbox.sh
@@ -18,8 +18,7 @@ sudo apt install \
 
 
 echo "Adicionando repositórios usados"
-sudo add-apt-repository ppa:webupd8team/terminix
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
