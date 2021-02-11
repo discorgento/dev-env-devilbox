aa6caeedd55c40289cfd928d4a4e0267e27aceec
Jonatan <web-jonatan@live.com>
2021-02-11 14:14:42 -0300

    Update and rename update_composer.sh to autostart.sh

---
+++ devilbox/autostart/autostart.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+#composer
+/usr/local/bin/composer self-update 1.10.19
+
+#n98
+wget https://files.magerun.net/n98-magerun2.phar \
+	&& chmod +x ./n98-magerun2.phar \
+	&& mv ./n98-magerun2.phar /usr/local/bin/
--- devilbox/autostart/update_composer.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-/usr/local/bin/composer self-update 1.10.19
