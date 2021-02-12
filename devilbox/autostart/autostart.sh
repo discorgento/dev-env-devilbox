5d24916fd281cd02347202ab69cf24861b99818e
Jonatan <web-jonatan@live.com>
2021-02-12 12:27:00 -0300

    Update autostart.sh

--- devilbox/autostart/autostart.sh
+++ devilbox/autostart/autostart.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
 #composer
-/usr/local/bin/composer self-update 1.10.19
+composer-1 global require --dev mage2tv/magento-cache-clean \
+	&& mv /root/.composer /home/devilbox/.composer
 
 #n98
 wget https://files.magerun.net/n98-magerun2.phar \
 	&& chmod +x ./n98-magerun2.phar \
 	&& mv ./n98-magerun2.phar /usr/local/bin/
-	
-#magento-cache-clean
-composer global require --dev mage2tv/magento-cache-clean
