0166adb0e1734e203e23f13c7159de6dd2022d75
Jonatan Machado <jonatanaxe@outlook.com>
2022-12-23 18:25:43 -0300

    Update autostart.sh

--- devilbox/autostart/autostart.sh
+++ devilbox/autostart/autostart.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #n98
-wget https://files.magerun.net/n98-magerun2.phar \
+curl -O https://files.magerun.net/n98-magerun2.phar \
 	&& chmod +x ./n98-magerun2.phar \
 	&& mv ./n98-magerun2.phar /usr/local/bin/
 
