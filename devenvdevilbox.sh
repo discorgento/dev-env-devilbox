ab7deec6b1af83ea0520306d9cd0cbd13f27b258
Jonatan Machado <jonatanaxe@outlook.com>
2022-12-23 18:24:52 -0300

    Update devenvdevilbox.sh

--- devenvdevilbox.sh
+++ devenvdevilbox.sh
@@ -4,7 +4,6 @@
 echo "Install Prerequisite"
 sudo apt install \
     curl \
-    wget \
     apt-transport-https \
     ca-certificates \
     software-properties-common \
@@ -12,95 +11,6 @@ sudo apt install \
 
 clear
 
-installapps(){
-
-    echo -n "
-    Which APP do you want to install?
-        (V)scode
-        (C)hrome
-        (T)ilix
-        (A)ll
-        (E)xit/I don't want to install apps
-"
-read answerapps
-case "$answerapps" in
-    v|V)
-        vscode
-        clear
-        echo "Installed vscode"
-        installapps
-    ;;
-    c|C)
-        chrome
-        clear
-        echo "Installed google chrome"
-        installapps
-    ;;
-    t|T)
-        tilix
-        clear
-        echo "Installed tilix"
-        installapps
-    ;;
-    a|A)
-        vscode
-        chrome
-        tilix
-        clear
-        echo "Installed vscode, chrome, tilix e spotify"
-    ;;
-    e|E)
-        clear
-        echo "Bye..."
-    ;;
-    *|"")
-        clear
-        echo "Invalid option"
-        installapps
-    ;;
-esac
-
-}
-
-
-tilix(){
-    clear
-    echo "***Installed tilix"
-    sudo apt -y install tilix
-}
-
-vscode(){
-    clear
-    echo "Add vscode repository? y/n"
-    read repovscode
-    if [ $repovscode = "y" ]
-    then
-        wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add --
-        sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-        sudo apt update
-        echo "***Vscode repository added"
-    else
-        echo "***Not Added Repository Vscode"
-    fi
-    echo "***Installing vscode"
-    sudo apt -y install code
-}
-
-chrome(){
-    clear
-    echo "Add chrome repository? y/n"
-    read repochrome
-    if [ $repochrome = "y" ]
-    then
-        wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-        sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-        sudo apt update
-        echo "***Chrome repository added"
-    else
-        echo "***Do not add repository Chrome"
-    fi
-    echo "***Installing google chrome"
-    sudo apt -y install google-chrome-stable
 }
 
 installenv(){
@@ -140,21 +50,21 @@ installenv(){
     git clone https://github.com/cytopia/devilbox.git
 
     cd ~/devilbox/
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/.env
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/docker-compose.override.yml
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/.env
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/docker-compose.override.yml
     mkdir sh
     cd ~/devilbox/sh
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/sh/docker-entrypoint-es.sh
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/sh/docker-entrypoint-es.sh
     cd ~/devilbox/bash/
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/bash/aliases.sh
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/bash/aliases.sh
     cd ~/devilbox/autostart/
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/autostart/autostart.sh
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/autostart/autostart.sh
     cd ~/devilbox/cfg/php-fpm-8.1/
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-fpm-x.x/www_server.conf
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-fpm-x.x/www_server.conf
     cd ~/devilbox/cfg/php-ini-8.1/
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-ini-x.x/memory_limit.ini
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-ini-x.x/timeouts.ini
-    wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-ini-x.x/xdebug.ini
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-ini-x.x/memory_limit.ini
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-ini-x.x/timeouts.ini
+    curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/cfg/php-ini-x.x/xdebug.ini
     cd ~/devilbox/
     
     clear
@@ -170,7 +80,7 @@ aliases(){
         source ~/.aliases
         fi" >> ~/.zshrc
         cd ~
-        wget -c https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/.aliases
+        curl -O https://raw.githubusercontent.com/Discorgento/dev-env-devilbox/main/devilbox/.aliases
         echo "***Aliases added"
     else
         echo "***Do not add Aliases"
@@ -210,7 +120,6 @@ VSCODE Docker -> https://marketplace.visualstudio.com/items?itemName=ms-azuretoo
 
 echo -n "
     What you want to install?
-        (S)oftware
         (D)evilBox
         (C)omplete
         (A)liases
@@ -218,15 +127,11 @@ echo -n "
 "
 read answerini
 case "$answerini" in
-    s|S)
-        installapps
-    ;;
     d|D)
         installenv
         welcomeenv
     ;;
     c|C)
-        installapps
         installenv
         aliases
         welcomeenv
