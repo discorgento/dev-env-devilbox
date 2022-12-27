dcabb4a3804d7b31676faef63351d659b3ba66cf
Jonatan Machado <jonatanaxe@outlook.com>
2022-12-27 15:38:57 -0300

    Update devenvdevilbox.sh

--- devenvdevilbox.sh
+++ devenvdevilbox.sh
@@ -85,7 +85,7 @@ pluginzsh(){
     then
         sudo usermod --shell $(which zsh) $USER
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-        git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
         git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
         
         echo "***Plugin zsh done!"
