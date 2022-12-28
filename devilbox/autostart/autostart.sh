9a8d0683c84afd4e5764ed554a8caf4d0021da3f
Jonatan Machado <jonatanaxe@outlook.com>
2022-12-28 09:06:04 -0300

    Update autostart.sh

--- devilbox/autostart/autostart.sh
+++ devilbox/autostart/autostart.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-#n98
-curl -L -o n98-magerun2.phar https://files.magerun.net/n98-magerun2.phar \
-	&& chmod +x ./n98-magerun2.phar \
-	&& mv ./n98-magerun2.phar /usr/local/bin/
-
 #composer
 composer-2 global require --dev mage2tv/magento-cache-clean \
 	&& mv /root/.composer /home/devilbox/.composer \
 	&& sudo chown -R devilbox /home/devilbox/.composer
+
+#n98
+curl -L -o n98-magerun2.phar https://files.magerun.net/n98-magerun2.phar \
+	&& chmod +x ./n98-magerun2.phar \
+	&& mv ./n98-magerun2.phar /usr/local/bin/
 	
